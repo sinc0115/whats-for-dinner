065bbc95a7d890ea341c459281c288afc0523e78
Katherine Sinclair <sinc0115@algonquinlive.com>
2020-11-20 14:40:17 -0500

    basic Material UI styling configured

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/styles'
 import './Header.css'
 
 import AppBar from '@material-ui/core/AppBar'
 import Typography from '@material-ui/core/Typography'
 
-class Header extends React.Component {
-  render () {
-    return (
-      <div>
-        <AppBar position='static'>
-          <Typography variant='h1'>What's for Dinner?</Typography>
-        </AppBar>
-      </div>
-    )
+const useStyles = makeStyles({
+  custom: {
+    color: 'pink',
+    fontWeight: 'bold'
   }
+})
+
+function Header() {
+    const classes = useStyles()
+
+    return (
+      <AppBar color='primary' position='static'>
+        <Typography variant='h1' align='center' className={classes.custom}>What's for Dinner?</Typography>
+      </AppBar>
+    )
 }
 
 export default Header
